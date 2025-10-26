2eaf93b8b66da61712f25e420d95290406c35d79
Vince Nibler <vintrepid@pm.me>
2025-10-26 08:09:40 -0700

    Fix: Remove duplicate FunWithFlags.Supervisor (auto-starts from config)

--- lib/maestro/application.ex
+++ lib/maestro/application.ex
@@ -1,6 +1,4 @@
 defmodule Maestro.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -10,27 +8,22 @@ defmodule Maestro.Application do
     children = [
       MaestroWeb.Telemetry,
       Maestro.Repo,
-      FunWithFlags.Supervisor,
+      {Phoenix.PubSub, name: Maestro.PubSub},
       {DNSCluster, query: Application.get_env(:maestro, :dns_cluster_query) || :ignore},
       {Oban,
        AshOban.config(
          Application.fetch_env!(:maestro, :ash_domains),
          Application.fetch_env!(:maestro, Oban)
        )},
-      {Phoenix.PubSub, name: Maestro.PubSub},
       Maestro.Ops.ProjectMonitor,
       MaestroWeb.Endpoint,
       {AshAuthentication.Supervisor, [otp_app: :maestro]}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Maestro.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
     MaestroWeb.Endpoint.config_change(changed, removed)
